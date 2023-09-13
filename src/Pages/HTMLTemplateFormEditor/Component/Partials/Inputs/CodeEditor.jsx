3dcaaac667878421f6481d4b6a53b65ed4427ad0
afdal-develop <afdal-develop@gmail.com>
2023-09-14 02:01:38 +0700

    Changes to be committed:   (use "git restore --staged <file>..." to unstage)         modified:   package-lock.json         modified:   package.json         modified:   src/Pages/HTMLTemplateFormEditor/Component/Partials/Inputs/CodeEditor.jsx

--- src/Pages/HTMLTemplateFormEditor/Component/Partials/Inputs/CodeEditor.jsx
+++ src/Pages/HTMLTemplateFormEditor/Component/Partials/Inputs/CodeEditor.jsx
@@ -5,10 +5,84 @@ import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/snippets/html";
-import { Button, Card, Col, Row, Typography } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Input,
+  Modal,
+  Row,
+  Select,
+  Tooltip,
+  Typography,
+} from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
+
+export function FormAddGenerateInput(props) {
+  const [form] = Form.useForm();
+
+  const formResetInput = () => {
+    form.setFieldsValue({ type: "text", name: "", label: "", value: "" });
+  };
+
+  return (
+    <Form
+      form={form}
+      onFinish={value => {
+        if (props.onSubmit) {
+          props.onSubmit(value, formResetInput);
+        }
+      }}
+      initialValues={{ type: "text", name: "", label: "", value: "" }}
+      layout="vertical"
+    >
+      <Form.Item required name="type" label="Type">
+        <Select
+          defaultValue={form.getFieldValue("type")}
+          options={[
+            { label: "TEXT", value: "text" },
+            { label: "IMAGE", value: "image" },
+            { label: "COLOR", value: "color" },
+          ]}
+        />
+      </Form.Item>
+
+      <Form.Item required name="name" label="Name" rules={[{ required: true }]}>
+        <Input placeholder="Name" />
+      </Form.Item>
+      <Form.Item
+        required
+        name="label"
+        label="Label"
+        rules={[{ required: true }]}
+      >
+        <Input placeholder="Label" />
+      </Form.Item>
+      <Form.Item
+        required
+        name="value"
+        label="Value"
+        rules={[{ required: true }]}
+      >
+        <Input placeholder="Value" />
+      </Form.Item>
+
+      <Row justify="end">
+        <Col>
+          <Button type="dashed" htmlType="submit" icon={<PlusCircleOutlined />}>
+            Add
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
 
 export default function CodeEditor({ value = "", onChange = value => {} }) {
+  const [newValue, setNewValue] = React.useState(value);
   const [cursor, setCursor] = React.useState(null);
+  const [showModalAddInput, setShowModalAddInput] = React.useState(false);
 
   const appendTextInCursor = newText => {
     const { $autoNewLine, $lines } = cursor.document;
@@ -20,8 +94,16 @@ export default function CodeEditor({ value = "", onChange = value => {} }) {
 
     $lines[row] = `${strBeforeColumn}${newText}${strAfterColumn}`;
 
-    onChange($lines.join($autoNewLine));
+    setNewValue($lines.join($autoNewLine));
   };
+
+  React.useEffect(() => {
+    onChange(newValue);
+  }, [newValue]);
+
+  React.useEffect(() => {
+    setNewValue(value);
+  }, [value]);
 
   const handleLoadAceEditor = editor => {
     editor.completers.push({
@@ -61,16 +143,44 @@ export default function CodeEditor({ value = "", onChange = value => {} }) {
   };
 
   return (
-    <Card title={<Row gutter={10} justify='space-between' align='middle'>
-      <Col>
-        <Typography.Text strong>CODE VIEW</Typography.Text>
-      </Col>
-      <Col>
-        <Row gutter={10}>
-          <Col><Button icon={} /></Col>
+    <Card
+      title={
+        <Row gutter={10} justify="space-between" align="middle">
+          <Col>
+            <Typography.Text strong>CODE VIEW</Typography.Text>
+          </Col>
+          <Col>
+            <Row gutter={10}>
+              <Col>
+                <Tooltip title="Add generate input from current cursor">
+                  <Button
+                    onClick={() => setShowModalAddInput(true)}
+                    icon={<PlusCircleOutlined />}
+                  >
+                    Add Input
+                  </Button>
+                </Tooltip>
+              </Col>
+            </Row>
+          </Col>
         </Row>
-      </Col>
-    </Row>}>
+      }
+    >
+      <Modal
+        title="Add generate input from current cursor"
+        open={showModalAddInput}
+        okButtonProps={{ style: { display: "none" } }}
+        cancelButtonProps={{ style: { display: "none" } }}
+        onCancel={() => setShowModalAddInput(false)}
+      >
+        <FormAddGenerateInput
+          onSubmit={(value, formResetInput) => {
+            appendTextInCursor(`{${JSON.stringify(value)}}`);
+            setShowModalAddInput(false);
+            formResetInput();
+          }}
+        />
+      </Modal>
       <AceEditor
         onLoad={handleLoadAceEditor}
         mode="html"
